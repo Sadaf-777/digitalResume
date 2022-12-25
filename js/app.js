86e3344f5898d3e8ecc4ee2b4707d0cc67620d2b
Tasneem sadaf <96732538+Sadaf-777@users.noreply.github.com>
2022-12-24 20:32:28 -0800

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,67 @@
+$(document).ready(function(){
+     
+    $('#profile__ripple').ripples({
+        resolution:512,
+        dropRadius:10
+
+    });
+   const bars=document.querySelectorAll('.progress__bar');
+   bars.forEach(function(bar){
+    let percentage=bar.dataset.percent;
+    let tooltip=bar.children[0];
+    tooltip.innerText=percentage + '%';
+    bar.style.width=percentage + '%';
+   })
+
+
+//    image filter
+    var $wrapper = $('.portfolio__wrapper');
+    // initialize isotope
+      $wrapper.isotope({
+        filter : '*',
+        layoutMode : 'masonry',
+        animationOptions :{
+            duration:750,
+            easing:'linear'
+        }
+
+      });
+      let links = document.querySelectorAll('.tabs a');
+       links.forEach(link => {
+        let selector =link.dataset.filter;
+         link.addEventListener('click',function(e){
+          e.preventDefault();
+          $wrapper.isotope({
+            filter : selector,
+            layoutMode : 'masonry',
+            animationOptions :{
+                duration:750,
+                easing:'linear'
+            }
+    
+          });
+          links.forEach(link =>{
+            link.classList.remove('active');
+          })
+
+            e.target.classList.add('active');
+
+         });
+       })
+      //  magnify popup
+      $('.magnify').magnificPopup({
+        type:'image',
+        gallery:{
+          enabled: true
+        },
+        zoom:{
+          enable:true
+        }
+      });
+      // slider
+      $('.slider').slick({
+        arrows:false,
+        autoplay:true
+
+      });
+});
